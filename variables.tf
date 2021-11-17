5a8c70a801ae7a073e965d70b58aed9fd5021567
Sandeep Raj <sandeep.raj@nium.com>
2021-11-17 11:40:02 +0530

    added

--- variables.tf
+++ variables.tf
@@ -1,12 +1,14 @@
 variable "aws_access_key" {
   description = "The AWS access key."
+  default = "AKIAUNUCME4AR2KJZWIB"
 }
 
 variable "aws_secret_key" {
   description = "The AWS secret key."
+  default = "6NGk31L0eFmwNHljl/1AYrBWnUVeGaTKuEeIyg6v"
 }
 
 variable "region" {
   description = "The AWS region to create resources in."
-  default = "eu-east-2"
+  default = "es-east-2"
 }
